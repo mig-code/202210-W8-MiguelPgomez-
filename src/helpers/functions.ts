8a6ebf4d8fc2cbc614c1eeb000ad339050c9d873
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-30 19:38:12 +0100

    Add comment to avoid code smell in sonar

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -4,6 +4,7 @@ export function filterByWatched(movies: Array<tvShowsType>, watched: boolean) {
     return movies.filter((movie) => movie.watched === watched);
 }
 
+//OLD WAY
 //This function is required if you want to add listeners from the movie.smart component
 
 // export function getFilmIdFromDom(index: number) {
